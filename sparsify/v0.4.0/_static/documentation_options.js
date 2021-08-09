f2198ce1d623dbf0778c65601625a6ae6b869796
Jenkins <jenkins@neuralmagic.com>
2021-08-09 12:21:32 +0000

    rebuild docs based on latest repos

--- sparsify/v0.4.0/_static/documentation_options.js
+++ sparsify/v0.4.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0.20210807',
+    VERSION: '0.4.0.20210809',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
