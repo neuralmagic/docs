5e5faeccd6c78d71e3f4f3967e208bae0a626031
Jenkins <jenkins@neuralmagic.com>
2021-10-18 12:24:02 +0000

    rebuild docs based on latest repos

--- sparsezoo/v.0.7.0/_static/documentation_options.js
+++ sparsezoo/v.0.7.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.0.20211016',
+    VERSION: '0.7.0.20211018',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- sparsezoo/v0.3.1/_static/documentation_options.js
+++ sparsezoo/v0.3.1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1.20211016',
+    VERSION: '0.3.1.20211018',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
