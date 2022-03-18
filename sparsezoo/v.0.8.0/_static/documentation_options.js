835f2f0f26d332849481688e38d9685dd2dd092e
Jenkins <jenkins@neuralmagic.com>
2022-03-18 12:36:25 +0000

    rebuild docs based on latest repos

--- sparsezoo/v.0.8.0/_static/documentation_options.js
+++ sparsezoo/v.0.8.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.0.20220317',
+    VERSION: '0.8.0.20220318',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
