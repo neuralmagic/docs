9872a10ca624add67c4c5d8c2884f6e20febc943
Jenkins <jenkins@neuralmagic.com>
2022-04-28 12:38:42 +0000

    rebuild docs based on latest repos

--- sparseml/v0.5.0/_static/documentation_options.js
+++ sparseml/v0.5.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.0.20220427',
+    VERSION: '0.5.0.20220428',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
