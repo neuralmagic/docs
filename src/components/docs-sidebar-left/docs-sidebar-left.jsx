24f05b8deadb17e58ec7997f4995bc1dbe98715c
Mark Kurtz <mark.kurtz@neuralmagic.com>
2022-07-07 15:56:56 -0400

    fix docs sidebar scrolling issues

--- src/components/docs-sidebar-left/docs-sidebar-left.jsx
+++ src/components/docs-sidebar-left/docs-sidebar-left.jsx
@@ -4,21 +4,38 @@ import styled from '@emotion/styled';
 import SidebarCollapsibleNav from './sidebar-collapsible-nav';
 
 
-const Container = styled.div`
+const Scroller = styled.div`
   width: calc(100% - 40px);
   height: calc(100% - 24px);
-  padding: 24px 40px 0 0;
+  margin-top: 24px;
+  padding-right: 40px;
+  overflow: scroll;
+
+  -ms-overflow-style: none;  /* Internet Explorer 10+ */
+  scrollbar-width: none;  /* Firefox */
+
+  ::-webkit-scrollbar {
+    display: none;  /* Safari and Chrome */
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: fit-content;
+  padding-bottom: 40px;
 `;
 
 
 const DocsSidebarLeft = ({currentId, nestedMdxEdges}) => {
   return (
-    <Container>
-      <SidebarCollapsibleNav nestedMdxEdges={nestedMdxEdges}
-                             currentId={currentId}
-                             rootLevel={true}
-                             pathIds={[]} />
-    </Container>
+    <Scroller>
+      <Container>
+        <SidebarCollapsibleNav nestedMdxEdges={nestedMdxEdges}
+                               currentId={currentId}
+                               rootLevel={true}
+                               pathIds={[]} />
+      </Container>
+    </Scroller>
   );
 }
 
