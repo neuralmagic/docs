068cd3bd75e0c882dc507ef8367c8546c4d13986
Mark Kurtz <mark.kurtz@neuralmagic.com>
2022-10-18 15:17:22 -0400

    Fix recipes improperly formatted for getting started sparsify

--- src/layouts/root.jsx
+++ src/layouts/root.jsx
@@ -46,7 +46,6 @@ const Root = ({ data, pageContext }) => {
   const isDocsPage = data && data.mdx;
   const metaTitle = data && data.mdx ? data.mdx.frontmatter.metaTitle : null;
   const metaDescription = data && data.mdx ? data.mdx.frontmatter.metaDescription : null;
-  console.log(pageContext);
 
   return (
     <RootDiv>
