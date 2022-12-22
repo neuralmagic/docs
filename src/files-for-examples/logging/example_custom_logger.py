c07c2dbd1e05f01307e02ef44d7ccc9e3c24d639
Robert Shaw <114415538+rsnm2@users.noreply.github.com>
2022-12-22 12:22:19 -0500

    Update src/files-for-examples/logging/example_custom_logger.py
    Co-authored-by: Jeannie Finks <74554921+jeanniefinks@users.noreply.github.com>

--- src/files-for-examples/logging/example_custom_logger.py
+++ src/files-for-examples/logging/example_custom_logger.py
@@ -7,7 +7,7 @@ class CustomLogger(BaseLogger):
         :param identifier: The name of the item that is being logged.
             By default, in the simplest case, that would be a string in the form
             of "<pipeline_name>/<logging_target>"
-            e.g. "image_classification/pipeline_inputs"
+            e.g., "image_classification/pipeline_inputs"
         :param value: The item that is logged along with the identifier
         :param category: The metric category that the log belongs to. 
             By default, we recommend sticking to our internal convention
