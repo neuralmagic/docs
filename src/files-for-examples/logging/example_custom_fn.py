23721e0a0280e5351756fb52cacfd5d164aa1d78
Robert Shaw <114415538+rsnm2@users.noreply.github.com>
2022-12-22 12:22:23 -0500

    Update src/files-for-examples/logging/example_custom_fn.py
    Co-authored-by: Jeannie Finks <74554921+jeanniefinks@users.noreply.github.com>

--- src/files-for-examples/logging/example_custom_fn.py
+++ src/files-for-examples/logging/example_custom_fn.py
@@ -1,7 +1,7 @@
 import numpy as np
 from typing import List
 
-# Engine inputs to transformers is 3 lists of np.arrays representing
+# Engine inputs to transformers are three lists of np.arrays representing
 # the encoded input, the attention mask, and token types.
 # Each of the np.arrays is of shape (batch, max_seq_len), so
 # engine_inputs[0][0] gives the encodings of the first item in the batch.
